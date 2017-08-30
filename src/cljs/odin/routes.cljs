456e09e0554fcf8fe738b6fba1aaf36801a7aaff
Derryl Carter <derrylcarter@gmail.com>
2017-08-29 17:01:40 -0700

    add support for url params in routes

--- src/cljs/odin/routes.cljs
+++ src/cljs/odin/routes.cljs
@@ -2,7 +2,10 @@
   (:require [accountant.core :as accountant]
             [bidi.bidi :as bidi]
             [re-frame.core :refer [dispatch reg-fx]]
-            [toolbelt.core :as tb]))
+            [toolbelt.core :as tb]
+            [cemerick.url :as c]
+            [clojure.walk :as walk]
+            [clojure.string :as string]))
 
 
 (def app-routes
@@ -20,6 +23,8 @@
                  ["/payments"
                   [[""         :profile.payment/history]
                    ["/sources" :profile.payment/sources]]]
+                   ;;["/sources" [["" :profile.payment/sources]
+                                ;;[["/" :source-id] :profile.payment.sources/entry]]]]]
                  ["/settings"
                   [["/change-password" :profile.settings/change-password]]]]]
 
@@ -42,6 +47,32 @@
   [])
 
 
+(def ^:private dummy-base
+  "http://foo.com")
+
+(defn baseify-uri
+  "Adds a fake protocol and domain to URI string,
+  for Cemerick's url function to interpret it properly."
+  [uri]
+  (if (nil? (re-find #"http" uri))
+    (str dummy-base uri)
+    uri))
+
+(defn unbaseify-uri
+  "Remove the thing."
+  [uri]
+  (string/replace uri (re-pattern dummy-base) ""))
+
+(defn uri
+  [uri]
+  (c/url (baseify-uri uri)))
+
+
+(defn parse-query-params
+  "Parses query parameters from a URL and yields them as a Clojure map."
+  [path]
+  (walk/keywordize-keys (:query (uri path))))
+
 
 (defn hook-browser-navigation!
   "Wire up the bidi routes to the browser HTML5 navigation."
@@ -50,16 +81,50 @@
    {:nav-handler  (fn [path]
                     (let [match  (bidi/match-route app-routes path)
                           page   (:handler match)
-                          params (:route-params match)]
+                          params (merge (parse-query-params path)
+                                        (:route-params match))]
                       (dispatch [:route/change page params])))
     :path-exists? (fn [path]
                     (boolean (bidi/match-route app-routes path)))})
   (accountant/dispatch-current!))
 
 
-(def path-for
+;;(def path-for
+;;  "Produce the path (URI) for `key`."
+;;  (partial bidi/path-for app-routes))
+
+
+;; (routes/path-for :profile/membership)
+;; (routes/path-for :profile/membership :account-id 1232455)
+;; (routes/path-for :profile/membership :account-id 1234566 :query-params {:something "foo"})
+
+;; /profile/membership/12345666
+;; /profile/membership/12345666?something=foo
+
+(defn append-query-params
+  [path params]
+  (if-let [query-params (:query-params params)]
+    (-> (uri path)
+        (assoc :query query-params)
+        str)
+    path))
+
+
+(defn get-route-params
+  [params]
+  (dissoc params :query-params))
+
+
+(defn path-for
   "Produce the path (URI) for `key`."
-  (partial bidi/path-for app-routes))
+  ([route]
+   (path-for route {}))
+  ([route & {:as params}]
+   (-> (bidi/path-for app-routes route (get-route-params params))
+       (append-query-params params)
+       unbaseify-uri)))
+
+  ;;(partial bidi/path-for app-routes))
 
 
 (reg-fx
