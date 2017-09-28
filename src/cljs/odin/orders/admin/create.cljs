628aca413d0884cebe8997422dad3304c1a496b5
Josh Lehman <jalehman37@gmail.com>
2017-09-27 21:58:19 -0700

    better wiring of modal, persist selections, prepare for svc card

--- src/cljs/odin/orders/admin/create.cljs
+++ src/cljs/odin/orders/admin/create.cljs
@@ -15,9 +15,9 @@
 
 
 (def default-db
-  {:showing  true
-   :accounts []
-   :services []})
+  {:accounts []
+   :services []
+   :form     {}})
 
 
 ;; =============================================================================
@@ -29,13 +29,6 @@
  ::create
  (fn [db _]
    (get db ::path)))
-
-
-(reg-sub
- ::showing?
- :<- [::create]
- (fn [db _]
-   (:showing db)))
 
 
 (reg-sub
@@ -52,6 +45,23 @@
    (:services db)))
 
 
+(reg-sub
+ ::form
+ :<- [::create]
+ (fn [db [_ k]]
+   (if (some? k)
+     (get-in db [:form k])
+     (:form db))))
+
+
+(reg-sub
+ ::selected-account
+ :<- [::accounts]
+ :<- [::form :account]
+ (fn [[accounts account-id] _]
+   (tb/find-by #(= account-id (:id %)) accounts)))
+
+
 ;; =============================================================================
 ;; Events
 ;; =============================================================================
@@ -60,19 +70,21 @@
 (reg-event-fx
  ::bootstrap
  (fn [{db :db} _]
-   {:db       (assoc db ::path default-db)
-    :dispatch [::fetch-autocomplete-data]}))
+   (let [bootstrapped (some? (::path db))]
+     {:db       (if bootstrapped db (assoc db ::path default-db))
+      :dispatch [::fetch-autocomplete-data bootstrapped]})))
 
 
 (reg-event-fx
  ::fetch-autocomplete-data
  [(path ::path)]
- (fn [{db :db} _]
-   {:dispatch [:loading ::fetch true]
-    :graphql  {:query      [[:accounts [:id :name :email [:property [:name]]]]
+ (fn [{db :db} [_ bootstrapped]]
+   (tb/assoc-when
+    {:graphql {:query      [[:accounts [:id :name :email [:property [:name]]]]
                             [:services [:id :code :name :desc :price :billed]]]
                :on-success [::fetch-success]
-               :on-failure [:graphql/failure ::fetch]}}))
+               :on-failure [:graphql/failure ::fetch]}}
+    :dispatch (when-not bootstrapped [:loading ::fetch true]))))
 
 
 (reg-event-fx
@@ -85,10 +97,10 @@
 
 
 (reg-event-db
- ::showing
+ ::update
  [(path ::path)]
- (fn [db [_ showing]]
-   (assoc db :showing showing)))
+ (fn [db [_ k v]]
+   (assoc-in db [:form k] v)))
 
 
 ;; =============================================================================
@@ -111,25 +123,28 @@
 
 
 (defn- account-autocomplete []
-  (let [accounts (subscribe [::accounts])]
+  (let [accounts (subscribe [::accounts])
+        account  (subscribe [::form :account])]
     [ant/auto-complete
      {:style             {:width "100%"}
       :placeholder       "search by name or email"
       :allow-clear       true
       :option-label-prop :label
+      :value             (str @account)
       :filter-option     (fn [val opt]
                            (let [q (string/lower-case (.. opt -props -qterm))]
                              (string/includes? q (string/lower-case val))))
-      :on-select         #(dispatch [::select-account (tb/str->int %)])}
+      :on-change         #(when (nil? %) (dispatch [::update :account nil]))
+      :on-select         #(dispatch [::update :account (tb/str->int %)])}
      (doall (map account-option @accounts))]))
 
 
 (defn- service-option [{:keys [id code name desc price]}]
   [autocomplete-option {:key   id
-                        :label name
+                        :label (string/replace name "&amp;" "&")
                         :qterm (str code " " name " " desc)}
    [:span
-    (or name code)
+    [:span {:dangerouslySetInnerHTML {:__html (or name code)}}]
     " - "
     (if-let [p price]
       (format/currency p)
@@ -138,36 +153,46 @@
 
 
 (defn- service-autocomplete []
-  (let [services (subscribe [::services])]
+  (let [services (subscribe [::services])
+        service  (subscribe [::form :service])]
     [ant/auto-complete
      {:style             {:width "100%"}
       :placeholder       "search within service name, code or description"
       :allow-clear       true
       :option-label-prop :label
+      :value             (str @service)
       :filter-option     (fn [val opt]
                            (let [q (string/lower-case (.. opt -props -qterm))]
                              (string/includes? q (string/lower-case val))))
-      :on-select         #(dispatch [::select-service (tb/str->int %)])}
+      :on-change         #(when (nil? %) (dispatch [::update :service nil]))
+      :on-select         #(dispatch [::update :service (tb/str->int %)])}
      (doall (map service-option @services))]))
 
 
 (defn- form []
-  [:div
-   [:div.field
-    [:label.label "For whom?"]
-    [account-autocomplete]]
-   [:div.field
-    [:label.label "Which service?"]
-    [service-autocomplete]]])
+  (let [form (subscribe [::form])]
+    (fn []
+      (let [{:keys [account service]} @form]
+        [:div
+         [:div.field
+          [:label.label "For whom?"]
+          [account-autocomplete]]
+         [:div.field
+          [:label.label "Which service?"]
+          [service-autocomplete]]
+
+         (when (and account service)
+           [:div.box.mt4
+            {:style {:min-height 100}}])]))))
 
 
 (defn- modal []
-  (let [is-showing (subscribe [::showing?])
+  (let [is-showing (subscribe [:modal/visible? ::modal])
         is-loading (subscribe [:loading? ::fetch])]
     [ant/modal
      {:title     "Create New Order"
       :visible   @is-showing
-      :on-cancel #(dispatch [::showing false])}
+      :on-cancel #(dispatch [:modal/hide ::modal])}
      [:div
       (if @is-loading
         [:div {:style {:text-align "center"
@@ -198,5 +223,5 @@
          [ant/button
           {:type     :primary
            :icon     "plus"
-           :on-click #(dispatch [::showing true])}
+           :on-click #(dispatch [:modal/show ::modal])}
           "Create Order"]])})))
