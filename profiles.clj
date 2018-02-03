2507923e85d100a4717c96e9a8dc95e430987692
Josh Lehman <jalehman37@gmail.com>
2018-02-03 14:47:41 -0800

    fixes to clojure 1.9.0 upgrade

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,18 @@
 {:dev {:source-paths ["src/clj" "src/cljs" "env/dev" "env/seed"]
-       :plugins      [[lein-figwheel "0.5.11" :exclusions [org.clojure/clojure org.clojure/core.async]]
+       :plugins      [[lein-figwheel "0.5.14" :exclusions [org.clojure/clojure org.clojure/core.async]]
                       [lein-cooper "1.2.2" :exclusions [org.clojure/clojure]]]
-       :dependencies [[figwheel-sidecar "0.5.11" :exclusions [ring/ring-core com.google.guava/guava]]
-                      [binaryage/devtools "0.9.4"]
+       :dependencies [[figwheel-sidecar "0.5.14" :exclusions [org.clojure/clojurescript
+                                                              ring/ring-core
+                                                              com.google.guava/guava]]
+                      [binaryage/devtools "0.9.9"
+                       :exclusions [org.clojure/clojurescript]]
                       [com.datomic/datomic-free "0.9.5544"]
-                      [devcards "0.2.3"]
-                      [starcity/reactor "1.4.0"]]
+                      [devcards "0.2.4"
+                       :exclusions [org.clojure/clojurescript
+                                    org.clojure/core.async]]
+                      [starcity/reactor "1.4.0"
+                       :excluions [ring cheshire clj-time org.apache.httpcomponents/httpcore commons-codec
+                                   org.clojure/core.async]]]
        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
        :cooper {"main" ["sass" "--watch" "-E" "UTF-8" "style/sass/main.sass:resources/public/assets/css/styles.css"]
@@ -30,11 +37,13 @@
                       :compiler     {:main             odin.core
                                      :optimizations    :advanced
                                      :elide-asserts    true
-                                     :pretty-print     false
+                                     :pretty-print     true
+                                     :pseudo-names     true
                                      :parallel-build   true
                                      :asset-path       "/js/cljs/out"
                                      :output-dir       "resources/public/js/cljs/out"
                                      :output-to        "resources/public/js/cljs/odin.js"
+                                     :source-map       "resources/public/js/cljs/odin.js.map"
                                      :externs          ["externs/stripe.ext.js"
                                                         "externs/highcharts.ext.js"]
                                      :closure-warnings {:externs-validation :off
@@ -46,11 +55,13 @@
                       :compiler     {:main             onboarding.core
                                      :optimizations    :advanced
                                      :elide-asserts    true
-                                     :pretty-print     false
+                                     :pretty-print     true
+                                     :pseudo-names     true
                                      :parallel-build   true
                                      :asset-path       "/js/cljs/onboarding/out"
                                      :output-dir       "resources/public/js/cljs/onboarding/out"
                                      :output-to        "resources/public/js/cljs/onboarding.js"
+                                     :source-map       "resources/public/js/cljs/onboarding.js.map"
                                      :externs          ["externs/stripe.ext.js"
                                                         "externs/chatlio.ext.js"]
                                      :closure-warnings {:externs-validation :off
