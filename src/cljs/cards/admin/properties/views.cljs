865f472fef8197e0f8992394137106c975c20437
Josh Lehman <jalehman37@gmail.com>
2018-02-13 16:50:52 -0800

    add rate input and form

--- src/cljs/cards/admin/properties/views.cljs
+++ src/cljs/cards/admin/properties/views.cljs
@@ -72,17 +72,63 @@ That's really it for the property overview. The interesting stuff is within
 the detail view of a property.
 ")
 
+
 (defcard-rg units-list
   "
 In the unit-centric view, we'll want to be able to view a list of units from which to select.
+
+From the list view, we can see much of the information relevant to units, including:
+
+- List of all units
+- Occupancy: vacant or member that lives there
+- When an occupying member is due to move out
 "
   (fn [data _]
     [:div.columns
      [:div.column.is-6
       [property/units-list
        {:units     sample-units
-        :page-size 15
+        ;; :page-size 15
         :active    (:active @data)
         :on-click #(swap! data assoc :active %)}]]])
   (r/atom {:active nil})
+  {:inspect-data true})
+
+
+(defcard-rg rate-input
+  "An input to be used to change/set the rate for a unit/property."
+  (fn [data _]
+    [property/rate-input
+     {:term      3
+      :value     @data
+      :on-change #(reset! data %)}])
+  (r/atom 2100)
+  {:inspect-data true})
+
+
+(defcard-rg rate-form
+  "A form of inputs to change/set the rates for a unit/property."
+  (fn [data _]
+    [:div
+     [:div.columns
+      [:div.column
+       [ant/form-item {:label "Toggle Loading"}
+        [ant/switch {:on-change #(swap! data update :is-loading not)}]]]
+
+      [:div.column
+       [ant/form-item {:label "Toggle Can Submit"}
+        [ant/switch {:default-checked true
+                     :on-change       #(swap! data update :can-submit not)}]]]]
+
+     [property/rate-form
+      {:rates      (:rates @data)
+       :is-loading (:is-loading @data)
+       :can-submit (:can-submit @data)
+       :on-change  (fn [id rate]
+                     (swap! data update :rates
+                            (fn [rates]
+                              (map #(if (= id (:id %)) (assoc % :rate rate) %) rates))))}]])
+  (r/atom {:rates      (map #(hash-map :term % :rate 2100 :id (seed/id)) [3 6 12])
+           :is-loading false
+           :can-submit true})
   {:inspect-data true})
