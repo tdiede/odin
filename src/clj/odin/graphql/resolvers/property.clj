2dbf3ac5b12c4e39bd25ba91ddae67fafd2acd15
Josh Lehman <jalehman37@gmail.com>
2018-02-13 22:38:57 -0800

    add graphql resolvers to set rates for properties & units

--- src/clj/odin/graphql/resolvers/property.clj
+++ src/clj/odin/graphql/resolvers/property.clj
@@ -1,10 +1,49 @@
 (ns odin.graphql.resolvers.property
   (:require [datomic.api :as d]
-            [blueprints.models.property :as property]))
+            [blueprints.models.property :as property]
+            [blueprints.models.license :as license]
+            [toolbelt.core :as tb]
+            [com.walmartlabs.lacinia.resolve :as resolve]
+            [blueprints.models.source :as source]))
 
 
 ;; ==============================================================================
-;; query resolvers===============================================================
+;; mutations ====================================================================
+;; ==============================================================================
+
+
+(defn update-existing [unit term rate]
+  (when-let [lp (tb/find-by (comp #{term} :license/term :license-price/license)
+                            (:property/licenses unit))]
+    {:db/id               (:db/id lp)
+     :license-price/price rate}))
+
+
+(defn create-new [db unit term rate]
+  {:db/id         (:db/id unit)
+   :unit/licenses {:license-price/price   rate
+                   :license-price/license (:db/id (license/by-term db term))}})
+
+
+(def allowed-term?
+  #{3 6 12})
+
+
+(defn set-rate!
+  "Set the rate for the property for the given term."
+  [{:keys [conn requester]} {:keys [id term rate]} _]
+  (when-not (allowed-term? term)
+    (resolve/resolve-as nil {:message (format "'%s' is not a valid term length!")
+                             :term    term}))
+  (let [property (d/entity (d/db conn) id)]
+    @(d/transact conn [(or (update-existing property term rate)
+                           (create-new property term rate))
+                       (source/create requester)])
+    (d/entity (d/db conn) id)))
+
+
+;; ==============================================================================
+;; queries ======================================================================
 ;; ==============================================================================
 
 
@@ -30,6 +69,8 @@
 
 (def resolvers
   {;; fields
+   ;; mutations
+   :property/set-rate! set-rate!
    ;; queries
-   :property/entry entry
-   :property/query query})
+   :property/entry     entry
+   :property/query     query})
