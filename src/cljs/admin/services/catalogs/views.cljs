aa103e27b15b1ada0a2bb99fcbb8d5cebbfa0268
Allen J Price <allen@starcity.com>
2018-02-26 18:53:12 -0800

    begins scaffolding catalogs view
    using hardcoded data

--- src/cljs/admin/services/catalogs/views.cljs
+++ src/cljs/admin/services/catalogs/views.cljs
@@ -10,5 +10,42 @@
 ;; subview ======================================================================
 ;; ==============================================================================
 
+(defn- controls []
+  [:div.columns
+   [:div.column.is-3
+    [ant/form-item {:label "Property"}
+     [ant/select
+      {:style         {:width "100%"}
+       :default-value "52gilbert"}
+      [ant/select-option {:value "52gilbert"} "52 Gilbert"]
+      [ant/select-option {:value "944market"} "944 Market"]
+      [ant/select-option {:value "611mission"} "611 Mission"]]]]
+   [:div.column.is-9.has-text-right
+    [ant/button
+     {:type :primary
+      :icon "plus"}
+     "New Service"]
+    [ant/button
+     {:type :primary
+      :icon "plus"}
+     "New Catalog"]]])
+
+
+
+(defn- content []
+  [:div.columns
+   [:div.column.is-3
+    [:h2 "Catalogs"]
+    [ant/menu
+     {:mode :vertical}
+     [ant/menu-item "Pets"]
+     [ant/menu-item "Laundry"]
+     [ant/menu-item "Storage"]
+     [ant/menu-item "Furniture"]]]
+   [:div.column
+    "and here will be the table"]])
+
 (defn subview []
-  [:h1 "This is the catalogs subview!"])
+  [:div
+   [controls]
+   [content]])
