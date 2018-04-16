b15019ad77dafa7c27c2f43bc05f5d304b61900b
therese <tdiede@gmail.com>
2018-04-15 22:39:16 -0700

    implement check resolvers

--- src/clj/odin/graphql/resolvers/check.clj
+++ src/clj/odin/graphql/resolvers/check.clj
@@ -8,11 +8,44 @@
             [datomic.api :as d]
             [blueprints.models.payment :as payment]))
 
+;; =============================================================================
+;; Fields
+;; =============================================================================
+
 
 (defn payment
-  "The payment that this check belongs to."
+  "The payment that this `check` belongs to."
   [_ _ check]
   (tcheck/payment check))
+
+
+(defn amount
+  "The amount of this `check`."
+  [_ _ check]
+  (tcheck/amount check))
+
+
+(defn name
+  "The name of the person who wrote this `check`."
+  [_ _ check]
+  (tcheck/name check))
+
+
+(defn received-on
+  "The date this `check` was received."
+  [_ _ check]
+  (tcheck/received-on check))
+
+
+(defn date
+  "The date this `check` was written."
+  [_ _ check]
+  (tcheck/date check))
+
+
+;; =============================================================================
+;; Mutations
+;; =============================================================================
 
 
 (defn create!
@@ -28,12 +61,22 @@
     (tpayment/add-check! payment' check-data)))
 
 
+;; =============================================================================
+;; Resolvers
+;; =============================================================================
+
+
 (defmethod authorization/authorized? :check/create! [_ account _]
   (account/admin? account))
 
 
 (def resolvers
-  {;; fields
-   :check/payment payment
+  {;;fields
+   :check/id          (fn [_ _ check] (tcheck/id check))
+   :check/payment     payment
+   :check/amount      amount
+   :check/name        name
+   :check/received-on received-on
+   :check/date        date
    ;; mutations
-   :check/create! create!})
+   :check/create!     create!})
