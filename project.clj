590a6ef1d6646a0a727008765bbdd8adfe52ca0b
Josh Lehman <jalehman37@gmail.com>
2018-04-17 12:54:04 -0700

    Version 1.9.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject odin "1.8.2-SNAPSHOT"
+(defproject odin "1.9.0-SNAPSHOT"
   :description "The all-dashboard."
   :url "http://my.starcity.com"
   :license {:name "Eclipse Public License"
